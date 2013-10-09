b34c317d46ac9b2019426691f725f7db4ab9dbea
Layton Miller <layton@desmill.com>
2013-10-08 19:23:13 -0700

    Upgraded calendar js functionality to only make requests when the target thumbnail hasn't already been clicked.

--- js/module/Tanguer_Calendar.js
+++ js/module/Tanguer_Calendar.js
@@ -19,14 +19,22 @@ Tanguer_Calendar.prototype = {
             e.preventDefault();
             var thumb = $(this).closest(".e.th");
             var eventID = thumb.data("event-id");
-            if(thumb.hasClass("selected"))
-                return false;
             var instance = $(this).closest(".c").attr("id");
             var data = {e:eventID};
             var ref = {scope:scope,instanceID:instance,eventID:eventID};
-
+            var li = $("#" + instance).find("li.c-e-disp.full[data-event-id='" + eventID + "']");
+            //Don't allow clicks on the presently selected element.
+            if(thumb.hasClass("selected"))
+                return false;
             thumb.siblings().removeClass("selected");
             thumb.addClass("selected");
+            //If the newly selected thumb has been selected already, show it and don't make a request.
+            if(li.length > 0){
+                scope.hideCurrentQuickEvent(instance);
+                li.show();
+                return false;
+            }
+            //All checks passed, we have to make the call.
             Tanguer_App.JSONCalls.getQuickEvent(data,scope.ajax_getQuickEventHandler,ref);
             return false;
         });
@@ -39,14 +47,9 @@ Tanguer_Calendar.prototype = {
         var calendar = $("#" + data.instanceID);
         var thumb = $("li.e.th[data-event-id=" + data.eventID + "]");
         this.body.trigger("Tanguer_Calendar_Quick_Event_Load", this);
-        var li = calendar.find("li.c-e-disp.full[data-event-id=" + data.eventID + "]");
-        if(li.length > 0){
-            li.show();
-            return;
-        }
-        li = $("<li>");
+        var li = $("<li>");
         li.addClass("c-e-disp full");
-        li.data("event-id",data.eventID);
+        li.attr("data-event-id",data.eventID);
         li.html(html);
         thumb.after(li);
     },
